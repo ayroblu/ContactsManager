cc51f560f1e0625e7865b31b32c40d8b2686f2c3
Ben Lu <ayroblu@gmail.com>
2022-06-06 01:06:28 +0100

    Add delete confirmation handling

--- Features/Contacts/ContactsView.swift
+++ Features/Contacts/ContactsView.swift
@@ -15,31 +15,80 @@ struct ContactsView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    @State private var searchText = ""
+//    @State private var isShowingConfirmationDialog = false
+//    @State private var deleteFunc: (() -> Void)?
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(items) { item in
-                    NavigationLink {
-                        ContactsDetailView(item: item)
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                    DeleteConfirmationView(buttonText: "Delete", confirmationText: "Delete Contact", action: {
+                        deleteItems(items: [item])
+                    }) {
+                        NavigationLink {
+                            ContactsDetailView(item: item)
+                        } label: {
+                            if let timestamp = item.timestamp {
+                                Text(timestamp, formatter: itemFormatter)
+                            } else {
+                                Text("deleting")
+                            }
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button("Edit Tags") {
+                                print("hi")
+                            }
+                        }
+                    }
+
+//                    NavigationLink {
+//                        ContactsDetailView(item: item)
+//                    } label: {
+//                        if let timestamp = item.timestamp {
+//                            Text(timestamp, formatter: itemFormatter)
+//                        } else {
+//                            Text("deleting")
+//                        }
+//                    }
+//                        .swipeActions(edge: .leading) {
+//                            Button("Edit Tags") {
+//                                print("hi")
+//                            }
+//                        }
+//                        .swipeActions {
+//                            Button("Delete", role: .destructive) {
+//                                deleteFunc = {
+//                                    deleteItems(items: [item])
+//                                }
+//                                isShowingConfirmationDialog = true
+//                            }
+//                        }
+                }
+//                    .confirmationDialog("Delete Contact", isPresented: $isShowingConfirmationDialog) {
+//                        Button("Delete Contact", role: .destructive) {
+//                            deleteFunc?()
+//                            isShowingConfirmationDialog = false
+//                        }
+//                    }
+                     // .onDelete(perform: deleteItems)
+            }
+                .navigationTitle("Contacts")
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+                .toolbar {
+    //                ToolbarItem(placement: .navigationBarTrailing) {
+    //                    EditButton()
+    //                }
+                    ToolbarItem {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            Text("Select a Contact")
         }
+        // https://stackoverflow.com/questions/65316497/swiftui-navigationview-navigationbartitle-layoutconstraints-issue
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 
     private func addItem() {
@@ -58,6 +107,20 @@ struct ContactsView: View {
         }
     }
 
+    private func deleteItems(items: [FetchedResults<Item>.Element]) {
+        withAnimation {
+            items.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
--- Features/Settings/SettingsView.swift
+++ Features/Settings/SettingsView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("TODO")
+        NavigationView {
+            Form {
+                Section(header: Text("Sync")) {
+                    Toggle(isOn: .constant(true), label: {Text("Sync")})
+                }
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
